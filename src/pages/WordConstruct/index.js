4fa345139857e37a9d9ae09be851ec8d89728580
Vitalii Haman <vitaliihaman@gmail.com>
2017-06-02 11:51:52 +0300

    add styles for  wordConstruction, small fixes

--- src/pages/WordConstruct/index.js
+++ src/pages/WordConstruct/index.js
@@ -1,25 +1,32 @@
 import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router-dom';
+import styles from './wordConstruct.css'
 
 class WordConstruct extends Component {
     static propTypes = {};
 
-    goToAdmin = () =>{
+    goToAdmin = () => {
 
     };
 
 
     render() {
         const word = "Колобординация";
-        return <div>Here will our constructor!
+        return <div><h1 className={styles.greeting}>Welcome to Word - Constructor!</h1>
             <Link to={'/admin'}>
-                <button >Go to AdminPanel</button>
+                <button className={styles.button}> Go to AdminPanel</button>
             </Link>
-            {word.split("").map(function (el, index) {
-                return <div key={index}>
-                    {el}
+            <div className={styles.wrapper}>
+                <div className = {styles.block}>
+                {word.split("").map(function (el, index) {
+                    return <div key={index} className={styles.letter}>
+                        {el}
+                    </div>
+                })}
                 </div>
-            })}
+            </div>
+            <button>Check word</button>
+            <button>Shake letter</button>
         </div>
     }
 }
